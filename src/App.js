1a4394e8e08171cdea3aadd849807443ad864625
Wrokbrook repo <chukschiboy@gmail.com>
2021-02-15 03:35:53 -0800

    preventing refresh on jsx with the setState line of code of event.preventDefault()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['Hello', 'Hi', 'What\'s up?']);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    // all logics to send a message goes in here
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, Chidiebere Chukwuma!</h1>
+      
+      {/* To activate the enter key on submit messages we have to wrap the entire elements in a form */}
+      
+      <form>
+
+        {/* input field */}
+        <input value={input} onChange={event => setInput(event.target.value)} />
+
+        {/* button */}
+        <button type='submit' onClick={sendMessage}>Send Message</button>
+
+      </form>
+      
+        {/* messages listed out */}
+        {
+          messages.map(message => (
+            <p>{message}</p>
+          ))
+        }
+
     </div>
   );
 }
