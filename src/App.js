b6b98263e7c20b72cca9bdcb93380ba2989af49a
Wrokbrook repo <chukschiboy@gmail.com>
2021-02-19 06:39:09 -0800

    update of the prompt box for username

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
 
 // personal imports
-import Message from '../src/Message';
+import Message from './Message';
 
 function App() {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState(['Hello', 'Hi', 'What\'s up?']);
+  const [messages, setMessages] = useState([
+    { username: 'ChidiChuks', text: 'hey guys' },
+    { username: 'Qazi', text: 'whats up?' }, 
+    { username: 'Sonny', text: 'hi peeps' }
+  ]);
+  const [username, setUsername] = useState('');
 
-  console.log(input);
-  console.log(messages);
+  // useState = is simply a stateful way of writing variables in React because it auto refreshes changes automatically
+  // useEffect =  is a block of code that is used to run conditional statements in React
+
+  useEffect(() => {
+    // run code here...
+    // if it's blank inside the [](dependencies), then this code runs ONCE when the app component loads
+    // if we have a variable like [input], it keeps running the block of code here
+    setUsername(prompt('Please enter your username...'));
+  }, 
+  // condition
+  []
+  );
 
   const sendMessage = (event) => {
     event.preventDefault();
     // all logics to send a message goes in here
-    setMessages([...messages, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput('');
   }
 
   return (
     <div className="App">
       <h1>Hello, Chidiebere Chukwuma!</h1>
+      <h2>Welcome {username}</h2>
       
       {/* To activate the enter key on submit messages we have to wrap the entire elements in a form */}
       
@@ -46,8 +62,7 @@ function App() {
         {/* messages listed out */}
         {
           messages.map(message => (
-            <Message />
-            <p>{message}</p>
+            <Message username={message.username} text={message.text} />
           ))
         }
 
